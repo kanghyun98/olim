937a0ebd1abf8f4b5c15b409f661968315508910
20170781 <lkh981107@naver.com>
2021-09-28 18:55:36 +0900

    Add basic settings

---
+++ front/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-use-before-define': 0,
+    'linebreak-style': 0,
+    'no-param-reassign': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
