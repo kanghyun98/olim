0d90f63e6fe46fdf14771d53c5a9b504eb1d9e5a
20170781 <lkh981107@naver.com>
2021-10-07 02:01:05 +0900

    feat: Add posts on profile page

--- front/src/pages/profile/[userName].tsx
+++ front/src/pages/profile/[userName].tsx
@@ -3,12 +3,70 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import ProfileHead from '../../components/ProfileHead';
+import PostItem from '../../components/PostItem';
 
 // 로그인되어 있다는 가정 하의 kanghyunee98의 프로필
 const dummy = {
   name: '이강현',
   userName: 'kanghyunee98',
-  Posts: [],
+  Posts: [
+    {
+      id: 1,
+      date: '2021-10-02',
+      content: '게시글 내용입니다.#test@kanghyunee98',
+      Images: [
+        { src: 'https://res.cloudinary.com/du2sma6fw/image/upload/v1629943639/default_image.jpg' },
+        { src: 'https://res.cloudinary.com/du2sma6fw/image/upload/v1629941392/home_image.jpg' },
+        { src: 'https://res.cloudinary.com/du2sma6fw/image/upload/v1629941392/home_image.jpg' },
+      ],
+      User: {
+        id: 1234,
+        userName: '강혀니',
+      },
+      Comments: [
+        {
+          User: {
+            id: 1107,
+            userName: '강현',
+          },
+          content: '첫번째 댓글입니다~ @kanghyun #test',
+        },
+        {
+          User: {
+            id: 1111,
+            userName: '강현2',
+          },
+          content: '두번째 댓글입니다~',
+        },
+      ],
+    },
+    {
+      id: 2,
+      date: '2021-10-02',
+      content: '게시글 두번째 내용입니다.',
+      Images: [{ src: 'https://res.cloudinary.com/du2sma6fw/image/upload/v1629941392/home_image.jpg' }],
+      User: {
+        id: 1235,
+        userName: '두번째 유저',
+      },
+      Comments: [
+        {
+          User: {
+            id: 1514,
+            userName: '강현123',
+          },
+          content: '첫번째 댓글입니12다1123123~',
+        },
+        {
+          User: {
+            id: 1235,
+            userName: '강현212424',
+          },
+          content: '두번째 댓글입니다~43443',
+        },
+      ],
+    },
+  ],
   Followings: [],
   Followers: [],
   isMyProfile: true,
@@ -36,6 +94,9 @@ const Profile = () => {
         followingsNum={followingsNum}
         isMyProfile={dummy.isMyProfile}
       />
+      {dummy.Posts.map((post) => {
+        return <PostItem key={post.id} post={post} />;
+      })}
     </>
   );
 };
