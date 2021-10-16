b27cedaa05cdcd99498d733bcdd6be256dc6eebc
20170781 <lkh981107@naver.com>
2021-10-16 23:42:47 +0900

    fix: Add ! that is definite assignment assertion on password for solving ts(2322)

--- back/src/database/config/config.ts
+++ back/src/database/config/config.ts
@@ -19,21 +19,21 @@ interface ConfigList {
 const config: ConfigList = {
   development: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD!,
     database: 'olim',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD!,
     database: 'olim',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD!,
     database: 'olim',
     host: '127.0.0.1',
     dialect: 'mysql',
