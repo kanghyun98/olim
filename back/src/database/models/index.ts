9e5229f04b877cda87aaf25f9f78017f2ee848c3
20170781 <lkh981107@naver.com>
2021-10-17 23:40:34 +0900

    fix: Remove calling password for security

--- back/src/database/models/index.ts
+++ back/src/database/models/index.ts
@@ -1,14 +1,14 @@
 import { Sequelize } from 'sequelize';
+
 import config from '../config/config';
 
 type EnvType = 'production' | 'test' | 'development';
 
 const env = (process.env.NODE_ENV as EnvType) || 'development';
-const { database, username, password } = config[env];
 
 export const sequelize = new Sequelize(
-  database,
-  username,
-  password,
+  config[env].database,
+  config[env].username,
+  config[env].password,
   config[env]
 );
