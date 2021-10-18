f01793e59086fe6379c388480b8731dc6890aafa
20170781 <lkh981107@naver.com>
2021-10-18 21:21:51 +0900

    feat: Make signup api

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,19 +2,23 @@ import express from 'express';
 
 import { sequelize } from './database/models/index';
 import postRouter from './routes/post';
+import userRouter from './routes/user';
 
 const app = express();
 const prod = process.env.NODE_ENV === 'production';
-
 app.set('port', prod ? process.env.PORT : 3065);
 
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(() => {
     console.log('db연결 성공');
   })
   .catch(console.error);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/user', userRouter);
 app.use('/post', postRouter);
 
 app.listen(app.get('port'), () => {
---
+++ back/src/routes/user.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+
+import User from '../database/models/user';
+
+const router = express.Router();
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    // 아이디 중복
+    const exUserId = await User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    });
+    // 사용자 이름 중복
+    const exUserName = await User.findOne({
+      where: {
+        userName: req.body.userName,
+      },
+    });
+
+    if (exUserId) {
+      return res.status(403).send('이미 사용중인 아이디입니다.');
+    } else if (exUserName) {
+      return res.status(403).send('이미 사용중인 username입니다.');
+    }
+
+    const hashedPasswrod = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      name: req.body.name,
+      userId: req.body.userId,
+      password: req.body.password,
+      userName: hashedPasswrod,
+    });
+    res.status(201).send('ok');
+  } catch (error) {
+    console.log(error);
+    next(error); // Express가 에러 처리 (status 500)
+  }
+});
+
+export default router;
