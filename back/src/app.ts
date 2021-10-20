4b6325d2968042b83017d6c1c5142355c592b5b5
20170781 <lkh981107@naver.com>
2021-10-20 22:56:12 +0900

    feat: add cookie and session settings

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import passport from 'passport';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
 import { sequelize } from './database/models/index';
 import postRouter from './routes/post';
 import userRouter from './routes/user';
 
+dotenv.config();
 const app = express();
 const prod = process.env.NODE_ENV === 'production';
+
 app.set('port', prod ? process.env.PORT : 3065);
 
 sequelize
@@ -22,8 +28,24 @@ app.use(
     credentials: true, // 쿠키
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // json 형식으로 데이터 받으면 req.body에 넣어줌
+app.use(express.urlencoded({ extended: true })); // form했을 때 데이터 처리??
+
+app.use(cookieParser(process.env.COOKIE_SECRET)); // cookie & session
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: false, // https이면 true
+      // domain: process.env.NODE_ENV === 'production' && '.주소.com'
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/user', userRouter);
 app.use('/post', postRouter);
