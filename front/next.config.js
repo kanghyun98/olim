86f7cb73186928a10e888e5cf02961be4a8c1e39
20170781 <lkh981107@naver.com>
2021-11-09 22:42:36 +0900

    chore: Add custom webpack

---
+++ front/next.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true, // gzip
+  webpack(config) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval', // 소스 코드 노출 방지
+      plugins: [...config.plugins],
+    };
+  },
+});
